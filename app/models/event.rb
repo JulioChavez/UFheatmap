c251fc2fb66df95e12e8e534ab93fb34e2910b2d
Hugh A. Miles II <hmiles23@ufl.edu>
2016-01-24 08:09:08 -0500

    fixes parameters for saving to DB

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,10 +1,8 @@
 class Event < ActiveRecord::Base
 	geocoded_by :address # replace location with the attribute of the address
 
-	#after_validation :geocode, if: ->(event){ event.address.present? and event.location_changed? }
-		after_validation :geocode, if: ->(event){ event.address.present? and event.location_changed? }
-
+	after_validation :geocode
 	def address
- 	 [street, city, state, zip_code].compact.join(', ')
+ 	 [street, city, state, ].compact.join(', ')
 	end
 end
