81ccf81b70bf694f8b66530ead9df8f4e674e521
blewis954 <blewis954@gmail.com>
2016-01-24 08:38:26 -0500

    sead the DB

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+event1 = Event.create(title: 'Made in Seed 1', street: '1864 STADIUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+event2 = Event.create(title: 'Made in Seed 1', street: '330 NEWELL DR', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+event3 = Event.create(title: 'Made in Seed 1', street: '1765 STADIUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
