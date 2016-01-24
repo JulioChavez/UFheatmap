63cada0fbb1b06ec44a37b1ccafb7b77a5c23119
blewis954 <blewis954@gmail.com>
2016-01-24 07:52:48 -0500

    minor changes

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,8 +1,10 @@
 class Event < ActiveRecord::Base
 	geocoded_by :address # replace location with the attribute of the address
 
-	after_validation :geocode, if: ->(event){ event.address.present? and event.location_changed? }
+	#after_validation :geocode, if: ->(event){ event.address.present? and event.location_changed? }
+		after_validation :geocode, if: ->(event){ event.address.present? and event.location_changed? }
+
 	def address
- 	 [street, city, state, country].compact.join(', ')
+ 	 [street, city, state, zip_code].compact.join(', ')
 	end
 end
