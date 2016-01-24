58568b9f45ebd811bf75849cbe5bd7edbed81982
Hugh A. Miles II <hmiles23@ufl.edu>
2016-01-24 08:48:34 -0500

    Added content to seed.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,3 +9,20 @@
 event1 = Event.create(title: 'Made in Seed 1', street: '1864 STADIUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
 event2 = Event.create(title: 'Made in Seed 2', street: '330 NEWELL DR', city: 'Gainesville', state:  'Florida', zip_code: '32611')
 event3 = Event.create(title: 'Made in Seed 3', street: '1765 STADIUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+
+event4 = Event.create(title: 'Made in Seed 4', street: '2001 MUSEUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+event5 = Event.create(title: 'Made in Seed 5', street: '330 NEWELL DR', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+event6 = Event.create(title: 'Made in Seed 6', street: '1765 STADIUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+
+
+Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
