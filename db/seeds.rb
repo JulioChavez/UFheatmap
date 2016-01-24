9c65d7f8c335d264599c935b85d1c596120977bc
Hugh A. Miles II <hmiles23@ufl.edu>
2016-01-24 12:18:06 -0500

    Final before demo

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,15 +6,13 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-event1 = Event.create(title: 'Made in Seed 1', street: '1864 STADIUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
-event2 = Event.create(title: 'Made in Seed 2', street: '330 NEWELL DR', city: 'Gainesville', state:  'Florida', zip_code: '32611')
-event3 = Event.create(title: 'Made in Seed 3', street: '1765 STADIUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+event1 = Event.create(title: 'Small Mid Day Banger', confirmed_attendees: '900', street: '1864 STADIUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+event2 = Event.create(title: 'Study Sesshhh', confirmed_attendees: '200', street: '330 NEWELL DR', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+event3 = Event.create(title: 'Super Smash Bros Tourney', confirmed_attendees: '3300', street: '1765 STADIUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
 
-event4 = Event.create(title: 'Made in Seed 4', street: '2001 MUSEUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
-event5 = Event.create(title: 'Made in Seed 5', street: '330 NEWELL DR', city: 'Gainesville', state:  'Florida', zip_code: '32611')
-event6 = Event.create(title: 'Made in Seed 6', street: '1765 STADIUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+event4 = Event.create(title: 'Flash Mob Meeting', confirmed_attendees: '500', street: '2001 MUSEUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+event5 = Event.create(title: 'Basketball Game', confirmed_attendees: '340', street: '330 NEWELL DR', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+event6 = Event.create(title: 'Clarifai Infosession',  confirmed_attendees: '2344',street: '1765 STADIUM RD', city: 'Gainesville', state:  'Florida', zip_code: '32611')
 
 
-Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
-Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
-Event.create(title: 'Made in Seed 1', street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
+Event.create(title: 'SWAMPHACKS!!!!', confirmed_attendees: '4000',street: '444 NEWELL DR ', city: 'Gainesville', state:  'Florida', zip_code: '32611')
