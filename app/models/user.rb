68789439aa06196a547470671709a3d5ef7a5f7c
Hugh A. Miles II <hmiles23@ufl.edu>
2016-02-16 13:54:25 -0500

    Revert "Added default user roles to all who signup"
    This reverts commit 45c61ccb2adb89b389b66f6c4215bdead2cc84bd.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,4 @@
 class User < ActiveRecord::Base
-  belongs_to :role
-  before_create :set_default_role
-  
   rolify
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
@@ -18,11 +15,6 @@ def self.koala(auth)
     access_token = auth['token']
     facebook = Koala::Facebook::API.new(access_token)
     #facebook.get_object("me?fields=name,picture")
-  end
-
-  private
-  def set_default_role
-    self.role ||= Role.find_by_name('attendee')
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206211101) do
+ActiveRecord::Schema.define(version: 20160124123140) do
 
   create_table "events", force: :cascade do |t|
     t.string   "title"
@@ -57,12 +57,10 @@ ActiveRecord::Schema.define(version: 20160206211101) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
-    t.integer  "role_id"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
-  add_index "users", ["role_id"], name: "index_users_on_role_id"
 
   create_table "users_roles", id: false, force: :cascade do |t|
     t.integer "user_id"
