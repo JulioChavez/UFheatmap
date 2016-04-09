548500e61aab6ac45b092eb3358223dfdfb5c5ae
hamt3ch <hmiles23@ufl.edu>
2016-04-09 14:57:38 -0400

    added test endpoint

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,2 +1,5 @@
 class ApiController < ApplicationController
+  def test
+    render :json => {message:"Leap ... and the net will appear"}, :status => 200
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -21,6 +21,7 @@ Rails.application.routes.draw do
 
   #match ':controller(/:action(/:id))', :via => :get
   get 'users/show'
+  get 'api/test'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
